7504f249ff55c196d957268825d4e7089fc668f0
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-12-05 10:32:18 +0100

    Created x and y properties for 'Entity' class.

--- src/entity.py
+++ src/entity.py
@@ -8,3 +8,11 @@ class Entity(object):
             raise(TypeError)
         self._x = x
         self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
