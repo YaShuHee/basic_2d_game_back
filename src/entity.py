0acdda5c31e1bba25a7ca7339014e208d628e5e0
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-12-05 09:23:15 +0100

    Implemented 'Entity.__init__' method.

--- src/entity.py
+++ src/entity.py
@@ -1,3 +1,10 @@
 class Entity(object):
     def __init__(self, x, y) -> None:
-        pass
+        """
+        The basic object that will be on the 2D game map.
+        An entity has an "x" and a "y" coordinates.
+        """
+        if not(isinstance(x, int) and isinstance(y, int)):
+            raise(TypeError)
+        self._x = x
+        self._y = y
