b464b856e870d89387441aeb1039dd5e065ec191
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-12-05 10:30:20 +0100

    Created tests for 'Entity.x' and 'Entity.y'.

--- test/test_entity.py
+++ test/test_entity.py
@@ -6,11 +6,14 @@ from src.entity import Entity
 
 
 class TestEntity(unittest.TestCase):
-    def test_entity_initiatisation_works_with_int_x_and_y(self):
-        entity = Entity(0, 0)
-        self.assertEqual(entity._x, 0)
-        self.assertEqual(entity._y, 0)
+    def test_entity_initialisation_works_with_int_x_and_y(self):
+        expected_x = 0
+        expected_y = 0
+        entity = Entity(expected_x, expected_y)
+        self.assertEqual(entity._x, expected_x)
+        self.assertEqual(entity._y, expected_y)
 
+    @staticmethod
     def load_x_y_initialisation_values_raising_type_error():
         return (
             ("0", "0"),
@@ -27,3 +30,25 @@ class TestEntity(unittest.TestCase):
     def test_entity_initialisation_raises_type_error_with_bad_type_x_or_y(self, x, y):
         with self.assertRaises(TypeError):
             entity = Entity(x, y)
+
+    @staticmethod
+    def load_x_and_y_int_values():
+        return (
+            (0, 0),
+            (42, 0),
+            (0, 42),
+            (442, 123),
+            (500, 500),
+            (-999, 0),
+            (0, -30),
+            (4, -12),
+            (-54, 2),
+            (-3, -7),
+            (-77, -77)
+        )
+    
+    @parameterized.expand(load_x_and_y_int_values())
+    def test_if_x_and_y_attributes_return_accurate_values(self, expected_x, expected_y):
+        entity = Entity(expected_x, expected_y)
+        self.assertEqual(entity.x, expected_x)
+        self.assertEqual(entity.y, expected_y)
