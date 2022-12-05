3f182b5490d5b042910f1e10dd0f0f06c5bac8b3
Timothé Gélibert <tim.gelibert@protonmail.com>
2022-12-05 12:16:21 +0100

    Implemented 'Map.__init__'.

--- src/map.py
+++ src/map.py
@@ -3,4 +3,9 @@ class Map(object):
         """
         The class used to control the map and the moving entities.
         """
-        pass
+        if not(isinstance(width, int) and isinstance(height, int)):
+            raise TypeError("width and height must be int")
+        if not(width > 0 and height > 0):
+            raise ValueError("width and height must be greater than or equal to 1")
+        self._width = width
+        self._height = height
